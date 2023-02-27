80873d0e09b2207732957f48edbb6cf42feff12e
domanhtung <Dotung.mto@gmail.com>
2023-02-27 17:21:24 +0700

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-blue.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <script src="script.js" defer></script>
