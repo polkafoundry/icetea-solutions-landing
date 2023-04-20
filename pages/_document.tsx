ecfc902684f6b16f819d73db62722a8f6f9c571b
domanhtung <Dotung.mto@gmail.com>
2023-04-20 10:14:47 +0700

    update icon svg to ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/icon.svg" />
+        <link rel="icon" href="/icon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
