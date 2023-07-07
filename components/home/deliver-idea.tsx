46413fe155b64e7bd78827f567529583cbc3b086
domanhtung <Dotung.mto@gmail.com>
2023-07-07 15:01:36 +0700

    update: Deliver your ideas to tech life

--- components/home/deliver-idea.tsx
+++ components/home/deliver-idea.tsx
@@ -7,6 +7,8 @@ import { Navigation } from "swiper";
 import { ArrowSwiperNext, ArrowSwiperPrev } from "../svg/home/arrow-swiper";
 import Model from "../global/model";
 import { useState } from "react";
+import Link from "next/link";
+import { pathname } from "@/constants";
 
 interface Props {
   title: string;
@@ -37,8 +39,8 @@ const DeliverIdea = ({ title }: Props) => {
               spaceBetween: 20,
             },
             1024: {
-              slidesPerView: 3,
-              spaceBetween: 48,
+              slidesPerView: 4,
+              spaceBetween: 32,
             },
           }}
           loop={true}
@@ -56,39 +58,28 @@ const DeliverIdea = ({ title }: Props) => {
             return (
               <SwiperSlide key={index} className="h-auto">
                 <div
-                  className="relative flex flex-col h-full cursor-pointer"
+                  className="relative flex p-1 flex-col h-full cursor-pointer"
                   onClick={() => handleShowModel(index)}
                 >
-                  <Image
-                    src={value?.img}
-                    className="w-full hl-[200px] xl:h-[333px]"
-                    width={657}
-                    height={501}
-                    alt="icon"
-                  />
                   <div
-                    className="p-5 xl:px-[30px] flex flex-col flex-1 xl:pb-[30px] xl:pt-6"
-                    style={{ boxShadow: "0px 4px 32px rgba(0, 0, 0, 0.08)" }}
+                    style={{
+                      boxShadow: "0px 6px 16px 0px rgba(0, 0, 0, 0.08)",
+                    }}
+                    className="p-10 h-[80vw] md:h-[208px] xl:h-[314px] flex items-center justify-center rounded-[12px]"
                   >
-                    <div className="xl:text-[20px] flex-1 text-[18px] xl:leading-[30px] text-[#0030C0] font-semibold capitalize">
-                      {value?.title}
-                    </div>
-                    <div className="grid grid-cols-2 gap-1 justify-between items-center mt-3 xl:mt-6">
-                      <div className="flex gap-1 items-center">
-                        {value?.flag}
-                        <div className="text-[12px] xl:text-[16px] text-[#0030C0]">
-                          {value?.position}
-                        </div>
-                      </div>
-                      <div className="flex gap-1 items-center">
-                        <div className="min-w-[23px]">
-                          <FeatherIcon />
-                        </div>
-                        <div className="text-[12px] xl:text-[16px] text-[#0030C0] truncate">
-                          {value?.tech}
-                        </div>
-                      </div>
-                    </div>
+                    <Image
+                      src={value?.img}
+                      className="w-full auto"
+                      width={657}
+                      height={501}
+                      alt="icon"
+                    />
+                  </div>
+                  <div className="text-[20px] xl:text-[32px] font-semibold pt-3 xl:pt-6 text-[#0030C0] text-center">
+                    {value?.title}
+                  </div>
+                  <div className="text-[14px] xl:text-[20px] text-[#0030C0] text-center">
+                    {value?.tag}
                   </div>
                 </div>
               </SwiperSlide>
@@ -103,46 +94,151 @@ const DeliverIdea = ({ title }: Props) => {
         </div>
       </div>
       <div className="overflow-hidden">
-        <button className="flex w-fit mt-6 lg:mt-12 gap-2 text-white items-center btn-fill-gradient px-6 py-2 mx-auto">
-          See more Project
-          <AboutUsArrowRight />
-        </button>
+        <Link href={`${pathname.SERVICE}/${pathname.BLOCKCHAINDEVELOPMENT}`}>
+          <button className="flex w-fit mt-6 lg:mt-12 gap-2 text-white items-center btn-fill-gradient px-6 py-2 mx-auto">
+            See more Project
+            <AboutUsArrowRight />
+          </button>
+        </Link>
       </div>
       <Model isOpen={isShow} handleClose={handleHide}>
-        <div style={{ maxWidth: "510px", width: "calc(100vw - 80px)" }}>
+        <div style={{ maxWidth: "980px", width: "calc(100vw - 80px)" }}>
           <div
-            className="relative flex flex-col h-full text-[#0030C0]"
+            className="relative flex flex-col h-full max-h-[90vh] text-[#0030C0] py-5 pl-5 lg:py-10 lg:pl-10"
             onClick={() => handleShow()}
           >
-            <Image
-              src={deliverYourIdea[indexSelect]?.img}
-              className="w-full hl-[200px] xl:h-[333px] rounded-[24px]"
-              width={657}
-              height={501}
-              alt="icon"
-            />
-            <div className="relative my-5 flex flex-col flex-1">
-              <div className="w-[1px] h-full bg-[#0030C0] absolute top-0 right-[2px]" />
-              <div className="lg:text-[34px] flex-1 text-[20px] lg:leading-[34px] font-semibold capitalize">
-                {deliverYourIdea[indexSelect]?.title}
-              </div>
-              <div className="flex gap-5 mt-3 xl:mt-6">
-                <div className="flex gap-1">
-                  {deliverYourIdea[indexSelect]?.flag}
-                  <div className="text-[12px] xl:text-[16px] font-semibold">
-                    {deliverYourIdea[indexSelect]?.position}
+            <div className="lg:max-h-[850px] overflow-y-auto custom-scrollbar">
+              <div className="grid grid-cols-3 gap-5 pr-5 py-4 lg:pr-10">
+                <div>
+                  <div className="flex h-[28vw] lg:h-[317px] p-10 items-center justify-center border-[#0083FF] border rounded-[12px]">
+                    <Image
+                      src={deliverYourIdea[indexSelect]?.img}
+                      className="w-full auto"
+                      width={657}
+                      height={501}
+                      alt="icon"
+                    />
                   </div>
+                  {!!deliverYourIdea[indexSelect]?.webImg &&
+                    typeof deliverYourIdea[indexSelect]?.webImg !==
+                      "undefined" && (
+                      <div className="mt-5">
+                        <Image
+                          src={deliverYourIdea[indexSelect]?.webImg || ""}
+                          className="w-full auto rounded-[2px]"
+                          width={657}
+                          height={501}
+                          alt="icon"
+                        />
+                      </div>
+                    )}
                 </div>
-                <div className="flex gap-1">
-                  <FeatherIcon />
-                  <div className="text-[12px] xl:text-[16px] font-semibold">
-                    {deliverYourIdea[indexSelect]?.tech}
+                <div className="col-span-2 text-[#0030C0]">
+                  <div className="flex gap-2 items-center flex-nowrap">
+                    <span className="text-[30px] lg:text-[48px] font-semibold text-[#0030C0]">
+                      {deliverYourIdea[indexSelect]?.title}
+                    </span>
+                    {deliverYourIdea[indexSelect]?.tag && (
+                      <span className="text-[8px] lg:text-[12px] py-1 px-2 lg:px-4 bg-[#0030C0] rounded-[16px] text-white">
+                        {deliverYourIdea[indexSelect]?.tag}
+                      </span>
+                    )}
                   </div>
-                </div>
-              </div>
-              <div className="mt-3 text-[14px] lg:text-[16px] max-h-[170px] overflow-auto custom-scrollbar">
-                <div className="pr-0 lg:pr-5">
-                  {deliverYourIdea[indexSelect]?.content}
+                  <div className="mt-3 lg:mt-5 text-[18px] lg:text-[24px] font-semibold">
+                    Business Goals
+                  </div>
+                  <div className="lg:text-[20px] mt-1 lg:mt-2">
+                    {deliverYourIdea[indexSelect]?.businessGoals}
+                  </div>
+                  <div className="mt-3 lg:mt-5 text-[18px] lg:text-[24px] font-semibold">
+                    Product Description
+                  </div>
+                  <div className="lg:text-[20px] mt-1 lg:mt-2">
+                    {deliverYourIdea[indexSelect]?.productDescription}
+                  </div>
+                  {deliverYourIdea[indexSelect]?.productDescription2 && (
+                    <div className="lg:text-[20px] mt-1 lg:mt-2">
+                      {deliverYourIdea[indexSelect]?.productDescription2}
+                    </div>
+                  )}
+                  {deliverYourIdea[indexSelect]?.productDescriptionList && (
+                    <ul className="lg:text-[20px] mt-1 lg:mt-2 list-disc pl-5">
+                      {deliverYourIdea[
+                        indexSelect
+                      ]?.productDescriptionList?.map(
+                        (item: string, index: number) => {
+                          return (
+                            <li
+                              className="lg:text-[20px] mt-1 lg:mt-2"
+                              key={index}
+                            >
+                              {item}
+                            </li>
+                          );
+                        }
+                      )}
+                    </ul>
+                  )}
+                  {deliverYourIdea[indexSelect]?.uniqueSellingPoint && (
+                    <>
+                      <div className="mt-3 lg:mt-5 text-[18px] lg:text-[24px] font-semibold">
+                        Unique Selling Points
+                      </div>
+                      <ul className="lg:text-[20px] mt-1 lg:mt-2 list-disc pl-5">
+                        {deliverYourIdea[indexSelect]?.uniqueSellingPoint?.map(
+                          (item: string, index: number) => {
+                            return (
+                              <li className="lg:text-[20px] mt-1 lg:mt-2" key={index}>
+                                {item}
+                              </li>
+                            );
+                          }
+                        )}
+                      </ul>
+                    </>
+                  )}
+                  {deliverYourIdea[indexSelect]?.teams && (
+                    <div className="flex text-[20px] mt-5 gap-2 items-end">
+                      <span className="font-semibold text-[18px] lg:text-[24px] lg:leading-[26px]">
+                        Team:{" "}
+                      </span>
+                      <span className="lg:leading-[22px] lg:text-[20px]">
+                        {deliverYourIdea[indexSelect]?.teams}
+                      </span>
+                    </div>
+                  )}
+                  {deliverYourIdea[indexSelect]?.developmentTime && (
+                    <div className="flex text-[20px] mt-5 gap-2 items-end">
+                      <span className="font-semibold text-[18px] lg:text-[24px] lg:leading-[26px]">
+                        Development Time:{" "}
+                      </span>
+                      <span className="lg:leading-[22px] lg:text-[20px]">
+                        {deliverYourIdea[indexSelect]?.developmentTime}
+                      </span>
+                    </div>
+                  )}
+                  <div className="mt-3 lg:mt-5 text-[18px] lg:text-[24px] font-semibold">
+                    Development Scope
+                  </div>
+                  <div className="lg:text-[20px] mt-1 lg:mt-2">
+                    {deliverYourIdea[indexSelect]?.developmentScope}
+                  </div>
+                  <div className="mt-3 lg:mt-5 text-[18px] lg:text-[24px] font-semibold">
+                    Applied Technologies
+                  </div>
+                  <div className="lg:text-[20px] mt-1 lg:mt-2">
+                    {deliverYourIdea[indexSelect]?.appliedTechnologies}
+                  </div>
+                  {deliverYourIdea[indexSelect]?.url && (
+                    <Link
+                      href={deliverYourIdea[indexSelect]?.url || ""}
+                      target={"_blank"}
+                    >
+                      <div className="w-fit mt-5 py-2 text-[12px] lg:text-[20px] px-6 border border-[#0030C0] rounded-[24px]">
+                        {deliverYourIdea[indexSelect]?.url}
+                      </div>
+                    </Link>
+                  )}
                 </div>
               </div>
             </div>
--- constants/home.tsx
+++ constants/home.tsx
@@ -310,31 +310,157 @@ export const engineeringsData = [
 
 export const deliverYourIdea = [
   {
-    img: "/images/home/banking_system.png",
-    title: "A comprehensive open banking system with APIs",
-    flag: <VnFlagIcon />,
-    position: "VietNam",
-    tech: "Java, AWS, React, Micro services",
-    content:
-      "An open banking API platform and system company based in the UK. It is the pioneer and innovator in the global Banking as a Platform (BaaP) sector, enabling banks, businesses and brands to define the future of consumer and SME finance. Their platform enables marketers, product managers, developers to take their financial product vision and rapidly prototype, launch and scale their vision using its open finance platform, financial operations, regulatory licensing and rich set of APIs.",
+    img: "/images/home/ideas/firebird.svg",
+    title: "Firebird",
+    webImg: "/images/home/ideas/firebird_web.png",
+    businessGoals: "Game-focused web2-friendly Layer 1 blockchain.",
+    productDescription:
+      "Firebird commit to allow developers to build scalable and frictionless Web3 Games with near-zero costs and never compromises your security.",
+    uniqueSellingPoint: [
+      "Guaranteed performance with a huge number of transactions per second.",
+      "Friendly and easy-to-use UX enabling features.",
+      "Scalability by executing zero-knowledge proof compatible smart contract.",
+      "Easily migratable.",
+      "Anti-cheat mechanism and security detection.",
+    ],
+    teams: "15 members",
+    developmentScope:
+      "Market research, In-depth planning, Product design, Coding, Testing, Deployment",
+    appliedTechnologies:
+      "Cosmos sdk, Tendermint BFT Consensus, Golang, Protobuf/gRPC",
+    url: "https://firebirdchain.com",
   },
   {
-    img: "/images/home/learning_platform.png",
-    title: "Online language learning platform",
-    flag: <JpFlagIcon />,
-    position: "Japan",
-    tech: "Java, Angular, React, Native Mobile App, AWS",
-    content:
-      "A platform offers a revolutionary new approach to language learning that combines live instruction with native teachers and multimedia-learning content customized to each learner. It allows students to study from home or the office and to avoid inconvenient scheduling, long commute times, high tuition costs, and outdated books.",
+    img: "/images/home/ideas/redkite.png",
+    title: "Red Kite",
+    tag: "Launchpad",
+    businessGoals: "A cutting-edge launchpad",
+    webImg: "/images/home/ideas/redkite_web.png",
+    productDescription:
+      "Red Kite Launchpad promises to give investors a new and surprising experience of an ordered and assured token sale process, with:",
+    productDescriptionList: [
+      "Lane-Based Swap System to assure fairness",
+      "Integrated Vesting Schedule",
+      "Reputation Point-Based Tier Ranking System",
+      "Multi-chain support",
+      "Pre-ordering Services and Refund tool.",
+    ],
+    developmentTime: "4 months",
+    developmentScope:
+      "Market Research, In-depth Planning, Product Design, Coding, Testing, Deployment, Post-production Maintenance, Product Operation.",
+    appliedTechnologies: "Node.js, React, Solidity, Redis, AWS.",
+    url: "https://redkitepad.com",
   },
   {
-    img: "/images/home/booking_service.png",
-    title: "Transportation booking service",
-    flag: <UkFlagIcon />,
-    position: "England",
-    tech: "Java, PHP, Angular, AWS",
-    content:
-      "A startup company in Australia, providing automatic garbage car book services and allowing to franchise their service systems.",
+    img: "/images/home/ideas/polkawallet.png",
+    title: "Polkasafe Wallet",
+    tag: "Wallet",
+    businessGoals: "Mobile Wallet",
+    webImg: "/images/home/ideas/polkawallet_app.png",
+    productDescription:
+      "Polkasafe wallet is designed to support Polkadot Auction Development.",
+    developmentTime: "3 months",
+    developmentScope:
+      "Market research, Product design, Coding, Testing, Deployment, Maintenance.",
+    appliedTechnologies: "React native, Blockchain wallet.",
+  },
+  {
+    img: "/images/home/ideas/gamefi.png",
+    title: "Gamefi.org",
+    tag: "Marketplace",
+    businessGoals: "An advanced vurtual plaza of web 2 digital collectibles.",
+    webImg: "/images/home/ideas/gamefi_web.png",
+    productDescription:
+      "A marketplace to manage NFT collections of all games via one virtual identity throughout the Gamefi.org ecosystem and other NFT marketplaces.",
+    developmentTime: "4 months",
+    developmentScope:
+      "Market research, In-depth planning, Product design, Coding, Testing, Deployment, Post-production maintenance, Product operation.",
+    appliedTechnologies:
+      "Dapps wyvern opensea, React nextjs, Tailwind CSS, NFT indexing.",
+    url: "https://gamefi.org/nft",
+  },
+  {
+    img: "/images/home/ideas/gamefi_mobile.png",
+    title: "Gamefi.org",
+    tag: "Mobile App",
+    businessGoals:
+      "A mobile app for thoes who take part in IDO, INO, Hub, Party throughout the Gamefi.org ecosystem anytime, anywhere.",
+    webImg: "/images/home/ideas/gamefi_mobile_app.png",
+    productDescription:
+      "Gamefi.org app allows users to explore verified insights of web3 games, and join exciting game events, through:",
+    productDescriptionList: [
+      "Push notifications to avoid missing events.",
+      "Allow IGO & INO on your phone effortlessly with high security.",
+      "Discover insights from 100+ popular web3 games.",
+      "Stay ahead of industry trends with reliable real-time data and updates.",
+      "Play mini-game and earn reward.",
+    ],
+    teams: "15 members",
+    developmentTime: "3 months",
+    developmentScope:
+      "Market research, In-depth planning, Product design, Coding, Testing, Deployment, Product operation, Maintenance.",
+    appliedTechnologies:
+      "React native (Back-end, Web Front-end, Mobile, 3rd Party services)",
+    url: "Https://apps.apple.com/vn/app/gamefi-org/id1599452056",
+  },
+  {
+    img: "/images/home/ideas/metaforce.png",
+    title: "Metaforce.org",
+    tag: "True Fans attraction tool",
+    businessGoals:
+      "The first web3 gaming on-demand work-to-earn protocol to attract real fans to project.",
+    productDescription:
+      " This marketing platform helps projects create autonomous campaign/event for their marketing (event, community, referral, reward, leader-board)",
+    productDescriptionList: [
+      "Bot-free mechanism",
+      "Rich type of tasks:",
+      "Social tasks (be active in social media to compete in event)",
+      "In-game tasks (play game to get high score and compete in event leader-board)",
+      "Bug-hunter task, quizzes",
+      "Event ranking and event reward strategy by defining bespoke data metrics",
+      "Flexible refering program, tracking and manage KPI of referral campaign to event.",
+    ],
+    teams: "15 members",
+    developmentTime: "3 months",
+    developmentScope:
+      "Market research, In-depth planning, Product design, Coding, Testing, Deployment",
+    appliedTechnologies:
+      "Typescript, React, NextJS, Tailwind CSS, Web3, MUI, AGGrid, Highcharts, StirybookNodeJS, Fastify, Postgres DB, Directus, FirebaseAWS, Google Cloud Platform, Github Actions.",
+  },
+  {
+    img: "/images/home/ideas/game_statistic.png",
+    title: "Game Statistics Service",
+    businessGoals:
+      "A multi tenants service, which collect and analyze offchain data for web3 project. tenant will have out of the box leader-board for their games.",
+    productDescription: "Multi tenant service, REST API",
+    teams: "4 members",
+    developmentTime: "2 months",
+    developmentScope:
+      "Requirement analysis, Product design, Coding, Testing, Deployment, Post-production Maintenance.",
+    appliedTechnologies:
+      "Typescript, NodeJS, Fastify, Clickhouse, Couchbase, Kafka, FirebaseAWS, Github Actions.",
+  },
+  {
+    img: "/images/home/ideas/metaforce_social.png",
+    title: "Metaforce Social API Service",
+    businessGoals:
+      "A multi tenants services, which connect to Telegram, Youtube, Facebook, Twitter, Discord to cllect and analyze user social interaction, then provide those data for tenant.",
+    productDescription: "Multi tenant service, REST API",
+    productDescription2:
+      "Tenant can query this service to check user status and behavior with some specific social data entity.",
+    productDescriptionList: [
+      "Check for membership of Telegram Group and Telegram Channel.",
+      "Check for membership of Youtube Channel subscription.",
+      "Check for membership of Facebook Page.",
+      "Check for membership of Twitter, following status, tweet and retweet, tweet content with hashtag, tweet like, tweet reply.",
+      "Check for membership of Discord server.",
+    ],
+    teams: "5 members",
+    developmentTime: "2,5 months",
+    developmentScope:
+      "Requirement analysis, Coding, Testing, Deployment, Post-production, Maintenance.",
+    appliedTechnologies:
+      "TypeScript, NodeJS, NestJS, Fastify, Postgres DB, FirebaseAWS, Google Cloud Platform, Github Actions.",
   },
 ];
 
